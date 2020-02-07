64dca470597d523babd16fb1c1d3c5372c7747b9
Jair Llamas <jair.llamas@tecmm.edu.mx>
2020-02-07 12:18:00 -0600

    modifique el enlace del moodle en el archivo enlaces.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Router from './router.js'
+import RouterPrincipal from './router.js'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router/>
+        <RouterPrincipal/>
       </div>
     );
   }
--- src/componentes/enlaces.js
+++ src/componentes/enlaces.js
@@ -18,8 +18,8 @@ class Enlaces extends Component {
        <div className="div-principalEnlaces">
         <h1>ENLACES</h1>
 
-        <a target="_blank" href="http://sycegrullo.sytes.net/">
-          <img src="http://tecmm.edu.mx/imagenesCampus/sice.png"/>
+        <a target="_blank" href="http://moodle.tecmm.edu.mx">
+          <img src="http://tecmm.edu.mx/imagenesCampus/moodle.png"/>
         </a>
 
         <a target="_blank" href="http://187.174.80.183:8080/core/login.jsp?panel=Aspirantes">
